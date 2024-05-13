62f0913d5844a88ed84007e36a81805d954e712b
Aflu <mohamedaflah186@gmail.com>
2024-05-13 17:27:39 +0530

    doc: Swagger metadata updated

--- server/src/utils/swagger/index.ts
+++ server/src/utils/swagger/index.ts
@@ -4,9 +4,9 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Design Planner - API Documentation',
+            title: 'Basyai trade  - API Documentation',
             version: '1.0.0',
-            description: 'Design Planner simplifies database design. Effortlessly manage multiple databases with automated code generation. Visualize and secure your data infrastructure. ( An Alternative for Code Planner, DbDesigner, etc... )',
+            description: 'Complete api documentation of Basyai trade api',
         },
     },
     apis: [
